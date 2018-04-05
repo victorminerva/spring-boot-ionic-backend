2981af340939af9c8554760767cedac84629c0ea
Victor Minerva <victorminerva.m@gmail.com>
2018-04-04 21:26:00 -0300

    Criando operação de instanciacao

--- src/main/java/com/victorminerva/cursomc/CursomcApplication.java
+++ src/main/java/com/victorminerva/cursomc/CursomcApplication.java
@@ -1,12 +1,30 @@
 package com.victorminerva.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.victorminerva.cursomc.domain.Categoria;
+import com.victorminerva.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categ1 = new Categoria(null, "Informática");
+		Categoria categ2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(categ1, categ2));
 	}
 }
