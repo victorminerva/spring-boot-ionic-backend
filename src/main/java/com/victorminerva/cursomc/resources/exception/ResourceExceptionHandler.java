ba6982320080a22f099b060bb1d5d909615e0b10
Victor Minerva <victorminerva.m@gmail.com>
2018-04-05 19:49:39 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/victorminerva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.victorminerva.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.victorminerva.cursomc.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), exception.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+}
--- src/main/java/com/victorminerva/cursomc/services/CategoriaService.java
+++ src/main/java/com/victorminerva/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.victorminerva.cursomc.domain.Categoria;
 import com.victorminerva.cursomc.repositories.CategoriaRepository;
+import com.victorminerva.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepo;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> categ = categoriaRepo.findById(id);
-		return categ.orElse(null);
+		return categ.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
