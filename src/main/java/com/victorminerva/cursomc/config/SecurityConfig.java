f33a865b66eff37aa81d07f43bae71989157db86
Victor Minerva <victorminerva.m@gmail.com>
2018-04-16 21:25:56 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/victorminerva/cursomc/config/SecurityConfig.java
+++ src/main/java/com/victorminerva/cursomc/config/SecurityConfig.java
@@ -7,24 +7,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.victorminerva.cursomc.security.JWTAuthenticationFilter;
+import com.victorminerva.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public static final String[] PUBLIC_MATCHERS = { "/h2-console**" };
-	public static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**",  "/clientes/**" };
+	public static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**"};
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -38,7 +49,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll()
 				.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(this.authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
---
+++ src/main/java/com/victorminerva/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.victorminerva.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.victorminerva.cursomc.domain.Cliente;
+import com.victorminerva.cursomc.repositories.ClienteRepository;
+import com.victorminerva.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cliente = clienteRepo.findByEmail(email);
+		if (cliente == null) {
+			throw new UsernameNotFoundException(email);
+		}
+
+		return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+	}
+
+}
