25d44e46816fa063c0bfd82684c74fbb5d1e7659
Victor Minerva <victorminerva.m@gmail.com>
2018-04-17 21:53:58 -0300

    Restrição de conteúdo: cliente recupera apenas seus pedidos

--- src/main/java/com/victorminerva/cursomc/resources/PedidoResource.java
+++ src/main/java/com/victorminerva/cursomc/resources/PedidoResource.java
@@ -5,12 +5,14 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,5 +39,15 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pedido.getId()).toUri();
 
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+		Page<Pedido> pages = pedidoService.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(pages);
 	}
 }
--- src/main/java/com/victorminerva/cursomc/services/PedidoService.java
+++ src/main/java/com/victorminerva/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.victorminerva.cursomc.domain.Cliente;
 import com.victorminerva.cursomc.domain.ItemPedido;
 import com.victorminerva.cursomc.domain.PagamentoComBoleto;
 import com.victorminerva.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.victorminerva.cursomc.domain.enums.EstadoPagamento;
 import com.victorminerva.cursomc.repositories.ItemPedidoRepository;
 import com.victorminerva.cursomc.repositories.PagamentoRepository;
 import com.victorminerva.cursomc.repositories.PedidoRepository;
+import com.victorminerva.cursomc.security.UserSS;
+import com.victorminerva.cursomc.services.exception.AuthorizationException;
 import com.victorminerva.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -73,5 +79,17 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(pedido);
 		
 		return pedido;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null ) {
+			throw new AuthorizationException("Acesso negado.");
+		}
+		
+		PageRequest pageReq = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.findById(user.getId());
+		
+		return pedidoRepo.findByCliente(cliente, pageReq);
 	}
 }
