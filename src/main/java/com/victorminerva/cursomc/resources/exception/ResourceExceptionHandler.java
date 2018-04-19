a1ccead01d8f180a8d7df8489be99b2d18bd4dfc
Victor Minerva <victorminerva.m@gmail.com>
2018-04-18 21:22:05 -0300

    Tratando exceções adequadamente

--- src/main/java/com/victorminerva/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/victorminerva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.victorminerva.cursomc.services.exception.AuthorizationException;
 import com.victorminerva.cursomc.services.exception.DataIntegrityException;
+import com.victorminerva.cursomc.services.exception.FileException;
 import com.victorminerva.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -23,27 +27,28 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException exception, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(),
 				System.currentTimeMillis());
-		
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exception, HttpServletRequest request) {
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException exception,
+			HttpServletRequest request) {
 		ValidationError error = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
 				System.currentTimeMillis());
 
-		for (FieldError each: exception.getBindingResult().getFieldErrors()) {
+		for (FieldError each : exception.getBindingResult().getFieldErrors()) {
 			error.addError(each.getField(), each.getDefaultMessage());
 		}
-		
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
-	
+
 	@ExceptionHandler(AuthorizationException.class)
 	public ResponseEntity<StandardError> authorization(AuthorizationException exception, HttpServletRequest request) {
 		StandardError error = new StandardError(HttpStatus.FORBIDDEN.value(), exception.getMessage(),
@@ -51,5 +56,38 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
 	}
+
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException exception, HttpServletRequest request) {
+		HttpStatus code = HttpStatus.valueOf(exception.getErrorCode());
+
+		StandardError error = new StandardError(code.value(), exception.getMessage(), System.currentTimeMillis());
+
+		return ResponseEntity.status(code).body(error);
+	}
+
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(),
+				System.currentTimeMillis());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazonS3(AmazonS3Exception exception, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(),
+				System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
 }
